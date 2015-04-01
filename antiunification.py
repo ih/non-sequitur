727d6edcb96f30c29ce42dd1c0ec091fcc5cc160
Irvin Hwang <irvin.hwang@gmail.com>
2015-04-01 14:23:58 +0800

    most of main function implemented, untested

---
+++ antiunification.py
@@ -0,0 +1,2 @@
+def find_best(function):
+    pass
--- non-sequitur.py
+++ non-sequitur.py
@@ -1,5 +1,20 @@
+import antiunification
+import unification
+
 # TODO add expression abstract class if necessary, make function, variable, etc
 # expressions
+APPLICATION_PLACEHOLDER = '*'
+
+
+def size(expression):
+    if type(expression) is list:
+        return sum([size(term) for term in expression])
+    else:
+        return 1
+
+
+def substitute():
+    pass
 
 
 class Symbol(object):
@@ -30,12 +45,47 @@ class Function(object):
         self.name = name
         self.parameters = parameters
         self.body = body
+        self.application_count = 0
         assert name not in Function.index
         Function.index[self.name] = self
 
     def check(self):
-        for function in Function.index.values().remove(self):
-            pass
+        # apply existing functions
+        best_substitution, new_body = self.find_best_unification()
+        if size(new_body) < size(self.body):
+            self.body = new_body
+            self.check()
+        else:
+            # create a new function if it helps
+            # here applications are
+            # {name: function_name, body: body_with_application}
+            new_parameters, new_body, application_self, application_other = (
+                self.find_best_antiunification())
+            other_function = Function.index[application_other['name']]
+            application_self_is_smaller = (
+                size(application_self['body']) < size(self.body))
+            application_other_is_smaller = (
+                size(application_other['body']) < size(other_function.body))
+            if application_self_is_smaller and application_other_is_smaller:
+                new_function = Function(
+                    parameters=new_parameters, body=new_body)
+                application_self_body = substitute(
+                    new_function.name, APPLICATION_PLACEHOLDER,
+                    application_self)
+                application_other_body = substitute(
+                    new_function.name, APPLICATION_PLACEHOLDER,
+                    application_other)
+                self.body = application_self_body
+                self.check()
+                other_function.body = application_other_body
+                other_function.check()
+        # remove any underutilized functions
+
+    def find_best_unification(self):
+        return unification.find_best(self)
+
+    def find_best_antiunification(self):
+        return antiunification.find_best(self)
 
     def __str__(self):
         string_parameters = [str(parameter) for parameter in self.parameters]
